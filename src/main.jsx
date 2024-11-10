ef4535e4b243d156b4778285ad112cef551a1c65
Asief Mahir <asiefmahir1@gmail.com>
2024-11-10 18:21:38 +0600

    feat: Learn about Fundamentals of Component and Reusable Components with Props

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
-// import { StrictMode } from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 // import "./index.css";
 import App from "./App.jsx";
 
-createRoot(document.getElementById("ourRoot")).render(<App />);
+createRoot(document.getElementById("ourRoot")).render(
+	<StrictMode>
+		<App />
+	</StrictMode>,
+);
 
 // React Dom -> Main Dom /HTML             -> React App
 
