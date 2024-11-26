cd6630db88b82d41e2a219dbe7fde636d18a0e2a
Asief Mahir <asiefmahir1@gmail.com>
2024-11-27 03:29:11 +0600

    feat: Learn about json-server package, crud with api

--- src/components/Button.jsx
+++
@@ -1,9 +0,0 @@
-import { memo } from "react";
-
-const Button = ({ clickHandler }) => {
-	console.log("Button is rendering");
-
-	return <button onClick={clickHandler}>Increase By 1</button>;
-};
-
-export default memo(Button);
--- src/components/OurAppTitle.jsx
+++
@@ -1,9 +0,0 @@
-import { memo } from "react";
-
-const OurAppTitle = () => {
-	console.log("OurAppTitle is rendering");
-
-	return <h2>Our Simple App</h2>;
-};
-
-export default memo(OurAppTitle);
